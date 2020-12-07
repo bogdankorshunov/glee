151e593d7d57ad370b74aa4b610e59f56ed45e0f
bogdankorshunov <crayzArtys@GMAIL.COM>
2020-12-08 03:33:55 +0500

    added home-products (of the week)

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,14 @@
 $(function () {
   $('.home-top-slider__inner').slick({
-      fade: true,
-      dots: true,
-      arrows: false,
-      autoplay: true,
-      autoplaySpeed: 7000
-  })
+    fade: true,
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 7000,
+  });
+  let mixer = mixitup($('.home-products__items'));
+  $('.filter-item').click(function(){
+    $('.filter-item').removeClass("filter-item--active");
+    $(this).addClass("filter-item--active");
+});
 });
