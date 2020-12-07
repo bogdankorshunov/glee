a649dad291728749a81e1d949d8245063a4f7620
bogdankorshunov <crayzArtys@GMAIL.COM>
2020-12-07 23:55:02 +0500

    added src folder

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+  $('.home-top-slider__inner').slick({
+      fade: true,
+      dots: true,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 7000
+  })
+});
---
+++ src/scss/js/main.js
@@ -0,0 +1,19 @@
+// // import mixitup from 'node_modules/mixitup/dist/mixitup.js'
+// import mixitup from 'mixitup';
+// $(function () {
+//   $(".home-top-slider__inner").slick({
+//     fade: true,
+//     dots: true,
+//     arrows: false,
+//   });
+//   $(".home-products__btn").on("click", function (e) {
+//     $(".home-products__btn").removeClass("filter__btn--active");
+//     $(this).addClass("filter__btn--active");
+//   });
+//   let mixer = mixitup($(".products"), {
+//     animation: {
+//       duration: 500,
+//       effects: "fade scale(0.9)",
+//     },
+//   });
+// });
